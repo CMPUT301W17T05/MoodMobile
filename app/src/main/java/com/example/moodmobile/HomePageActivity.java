b57bbed98f7066f4c809a77be13cb02c0086c722
szindi <zindi@ualberta.ca>
2017-03-09 15:51:27 -0700

    add variables and imports

--- app/src/main/java/com/example/moodmobile/HomePageActivity.java
+++ app/src/main/java/com/example/moodmobile/HomePageActivity.java
@@ -1,10 +1,35 @@
 package com.example.moodmobile;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    final Context context = this;
+    final ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
