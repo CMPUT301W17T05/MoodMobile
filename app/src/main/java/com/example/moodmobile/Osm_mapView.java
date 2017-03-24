de94a47aa8544a12b38909dc7dc2f8ba22310097
HaozhouPang <haozhou@ualberta.ca>
2017-03-23 19:20:40 -0600

    get current location

--- app/src/main/java/com/example/moodmobile/Osm_mapView.java
+++ app/src/main/java/com/example/moodmobile/Osm_mapView.java
@@ -16,19 +16,26 @@ import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import java.util.ArrayList;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import android.util.Log;
+
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 
-public class Osm_mapView extends AppCompatActivity {
+public class Osm_mapView extends AppCompatActivity implements LocationListener {
     private MapView         MapView;
     private MapController   MapController;
-    ArrayList<OverlayItem> OverlayItemArray;
-
+    private  LocationManager locationManager;
+    private Location location; // Location
+    private double latitude; // Latitude
+    private double longitude; // Longitude
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    ArrayList<OverlayItem> overlayItemArray;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,26 +46,61 @@ public class Osm_mapView extends AppCompatActivity {
         MapView.setBuiltInZoomControls(true);
         MapView.setMultiTouchControls(true);
         MapController = (MapController) MapView.getController();
-        MapController.setZoom(9);
-        GeoPoint gPt = new GeoPoint(53.5444, -113.4909);
-        MapController.setCenter(gPt);
+        MapController.setZoom(13);
+        overlayItemArray = new ArrayList<OverlayItem>();
 
-        OverlayItemArray = new ArrayList<OverlayItem>();
-        OverlayItemArray.add(new OverlayItem("Canada", "Edmonton", gPt));
+        GeoPoint center = new GeoPoint(53.34, -113.9);
+        MapController.animateTo(center);
+        addMarker(center);
 
-        ItemizedIconOverlay<OverlayItem> ItemizedIconOverlay
-                = new ItemizedIconOverlay<OverlayItem>(
-                this, OverlayItemArray, null);
 
-        MapView.getOverlays().add(ItemizedIconOverlay);
 
-        ScaleBarOverlay myScaleBarOverlay = new ScaleBarOverlay(MapView);
-        MapView.getOverlays().add(myScaleBarOverlay);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, this);
+
 
     }
 
     protected void onStart() {
         super.onStart();
+    }
+
+
+    public void addMarker (GeoPoint center){
+        Marker marker = new Marker(MapView);
+        marker.setPosition(center);
+        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
+        marker.setIcon(getResources().getDrawable(R.drawable.common_google_signin_btn_icon_dark));
+
+        MapView.getOverlays().clear();
+        MapView.getOverlays().add(marker);
+        MapView.invalidate();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        GeoPoint center = new GeoPoint(location.getLatitude(), location.getLongitude());
+
+        MapController.animateTo(center);
+        addMarker(center);
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
 
