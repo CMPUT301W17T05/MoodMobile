c3ae97749c5b963413c499a82a4b4d558f6bfe87
Shingai Zindi <zindi@cs.ualberta.ca>
2017-04-03 11:02:56 -0600

    structure jobservices

--- app/src/main/java/com/example/moodmobile/SyncJobService.java
+++ app/src/main/java/com/example/moodmobile/SyncJobService.java
@@ -7,18 +7,30 @@ import android.app.job.JobService;
  * Created by shingai on 02/04/17.
  */
 
-public class SyncJobService extends JobService {
+public abstract class SyncJobService extends JobService {
     private static final String TAG = SyncJobService.class.getSimpleName();
 
     @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+    
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+    
+    @Override
     public boolean onStartJob(final JobParameters params) {
-
-        return false;
+        Log.i("JobService", "Started at:" + SystemClock.elapsedRealtime());
+        String json = job.getExtras().getString("mood");
+        Gson gson = new Gson();
+        Mood mood = gson.fromJson(json, Mood.class);
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
+        Log.i("JobService", "Stopped at:" + SystemClock.elapsedRealtime());
 
-        return false;
     }
 }
